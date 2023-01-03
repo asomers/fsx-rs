e695f59cbff363b92d35d9dcf74961d7d596b1f0
Alan Somers <asomers@gmail.com>
2023-01-02 17:58:58 -0700

    WIP initial implementation

---
+++ src/main.rs
@@ -0,0 +1,270 @@
+// vim: tw=80
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Seek, SeekFrom, Write},
+    num::Wrapping,
+    path::PathBuf,
+    process
+};
+
+use log::{debug, error, log_enabled, info, Level};
+use rand::{         
+    Rng,            
+    RngCore,
+    SeedableRng,
+    distributions::{Distribution, Standard, WeightedIndex},
+    thread_rng
+};  
+use rand_xorshift::XorShiftRng;
+
+use clap::Parser;
+
+const MAXFILELEN: u64 = 256 * 1024;
+const MAXOPLEN: usize = 64 * 1024;
+
+#[derive(Debug, Parser)]
+//#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// File name to operate on
+    fname: PathBuf,
+
+    /// Beginning operation number
+    #[arg(short = 'b', default_value_t = 1u64)]
+    opnum: u64,
+
+    /// 1/P chance of file close+open at each op (default infinity)
+    #[arg(short = 'c', value_name = "P")]
+    closeprob: Option<u64>,
+
+    /// Total number of operations to do (default infinity)
+    #[arg(short = 'N')]
+    numops: Option<u64>,
+    /// Seed for RNG
+    #[arg(short = 'S')]
+    seed: Option<u64>
+    // TODO
+    // -d
+    // -i
+    // -l
+    // -m
+    // -n
+    // -o
+    // -p
+    // -q
+    // -r
+    // -s
+    // -t
+    // -w
+    // -D
+    // -L
+    // -O
+    // -P
+    // -S
+    // -W
+    // -R
+    // -U
+
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Op {
+    Read,
+    Write,
+    MapRead,
+    Truncate,
+    MapWrite
+}
+
+impl Distribution<Op> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Op {
+        match rng.gen_range(0..=4) {
+            0 => Op::Read,
+            1 => Op::Write,
+            2 => Op::MapRead,
+            3 => Op::Truncate,
+            4 => Op::MapWrite,
+            _ => unreachable!()
+        }
+    }
+}
+struct Exerciser {
+    /// Current file size
+    file_size: u64,
+    fname: PathBuf,
+    // What the file ought to contain
+    good_buf: Vec<Wrapping<u8>>,
+    numops: Option<u64>,
+    opnum: u64,
+    // File's original data
+    original_buf: Vec<Wrapping<u8>>,
+    // Use XorShiftRng because it's deterministic and seedable.
+    // XXX It might be nicer to use random(3) for full compatibility with the C
+    // implementation.
+    rng: XorShiftRng,
+    seed: u64,
+    // Number of steps completed so far
+    steps: u64,
+    file: File
+}
+
+impl Exerciser {
+    fn check_buffers(&self, buf: &[u8], offset: u64) {
+        let size = buf.len();
+        if &self.good_buf[offset as usize..offset as usize + size] != &buf[..] {
+            error!("miscompare: offset= {:#x}, size = {:#x}", offset, size);
+            // TODO: detailed comparison
+            process::exit(0);
+        }
+    }
+
+    fn doread(&mut self, offset: u64, size: usize ) {
+        if size == 0 {
+            debug!("{} skipping zero size read", self.steps);
+            return;
+        }
+        if size as u64 + offset > self.file_size {
+            debug!("{} skipping seek/read past EoF", self.steps);
+        }
+        info!("{} read {:#x} thru {:#x} ({:#x} bytes)", self.steps,
+            offset,
+            offset + size as u64,
+            size);
+        self.file.seek(SeekFrom::Start(offset)).unwrap();
+        let mut temp_buf = vec![0u8; size];
+        let read = self.file.read(&mut temp_buf[..]).unwrap();
+        if read < size {
+            error!("short read: {:#x} bytes instead of {:#x}", read, size);
+            process::exit(1);
+        }
+        // TODO: check buffers
+    }
+
+    fn dowrite(&mut self, offset: u64, size: usize) {
+        if size == 0 {
+            debug!("{} skipping zero size write", self.steps);
+            return;
+        }
+
+        info!("{} write {:#x} thru {:#x} ({:#x} bytes)", self.steps,
+            offset,
+            offset + size as u64,
+            size);
+
+        self.gendata(offset, size);
+
+        if self.file_size < offset + size as u64 {
+            if self.file_size < offset {
+                safemem::write_bytes(&mut self.good_buf[self.file_size as usize ..offset as usize], 0)
+            }
+            self.file_size = offset + size as u64;
+        }
+        self.file.seek(SeekFrom::Start(offset)).unwrap();
+        let written = self.file.write(&self.good_buf[offset as usize..offset as usize + size]).unwrap();
+        if written != size {
+            error!("short write: {:#x} bytes instead of {:#x}", written, size);
+            process::exit(1);
+        }
+    }
+
+    fn exercise(&mut self) {
+        loop {
+            if let Some(n) = self.numops {
+                if n <= self.steps {
+                    break;
+                }
+            }
+            self.step();
+        }
+
+        println!("All operations completed A-OK!");
+    }
+
+    fn gendata(&mut self, offset: u64, mut size: usize) {
+        let mut uoff = usize::try_from(offset).unwrap();
+        loop {
+            size -= 1;
+            if size == 0 {
+                break;
+            }
+            self.good_buf[uoff] = (self.steps % 256) as u8;
+            if uoff % 2 > 0 {
+                self.good_buf[uoff] += self.original_buf[uoff];
+            }
+            uoff += 1;
+        }
+    }
+
+    fn step(&mut self) {
+        let op: Op = self.rng.gen();
+        self.steps += 1;
+
+        let mut size = MAXOPLEN;
+        let mut offset: u64 = self.rng.gen();
+        if op == Op::Write || op == Op::MapWrite {
+            offset %= MAXFILELEN;
+            if offset + size as u64 > MAXFILELEN {
+                size = usize::try_from(MAXFILELEN - offset).unwrap();
+            }
+            if op == Op::MapWrite {
+                todo!()
+            } else {
+                self.dowrite(offset, size);
+            }
+        } else {
+            offset = if self.file_size > 0 {
+                offset % self.file_size
+            } else {
+                0
+            };
+            if offset + size as u64 > MAXFILELEN {
+                size = usize::try_from(self.file_size - offset).unwrap();
+            }
+            if op == Op::MapRead {
+                todo!()
+            } else {
+                self.doread(offset, size);
+            }
+        }
+    }
+}
+
+impl From<Cli> for Exerciser {
+    fn from(cli: Cli) -> Self {
+        let seed = cli.seed.unwrap_or_else(|| {
+            //let mut seed = [0u8; 16];
+            let mut seeder = thread_rng();
+            //seeder.fill_bytes(&mut seed);
+            //seed
+            seeder.gen()
+        });
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&cli.fname)
+            .expect("Cannot create file");
+        let mut original_buf = vec![Wrapping::<u8>(80); MAXFILELEN as usize];
+        let mut good_buf = vec![Wrapping::<u8>(0); MAXFILELEN as usize];
+        let mut rng = XorShiftRng::seed_from_u64(seed);
+        rng.fill_bytes(&mut original_buf[..]);
+        Exerciser{
+            file,
+            file_size: 0,
+            fname: cli.fname,
+            good_buf,
+            numops: cli.numops,
+            opnum: cli.opnum,
+            original_buf,
+            rng,
+            seed,
+            steps: 0
+        }
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let mut exerciser = Exerciser::from(cli);
+    env_logger::init();
+    exerciser.exercise()
+}
